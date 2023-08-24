b97b948aa2057f98033d6cd6c88359758f59d9bc
Isaac Faraday <joseheraz04@outlook.com>
2023-08-24 12:12:24 -0600

    v2408231212

--- assets/navbar/js/navbar.js
+++ assets/navbar/js/navbar.js
@@ -26,10 +26,10 @@ const navTemplate = `
         
           <ul class="menuN">
             <li class="linkN">
-              <a href="https://manuelheraz.github.io/pages/listaMedallas/index.html#lista">Lista de Medallas</a>
+              <a href="https://manuelheraz.github.io/Medallas/pages/listaMedallas/index.html#lista">Lista de Medallas</a>
             </li>
             <li class="linkN">
-              <a href="manuelheraz.github.io/pages/cuadroCondecoraciones/index.html">Cuadro de condecoraciones</a> 
+              <a href="https://manuelheraz.github.io/Medallas/pages/cuadroCondecoraciones/index.html">Cuadro de condecoraciones</a> 
             </li>
           </ul>
         </li>
