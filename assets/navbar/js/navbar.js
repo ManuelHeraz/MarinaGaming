5aa24034bbec5753585c1b85174a351a650662b6
Isaac Faraday <joseheraz04@outlook.com>
2023-08-26 13:49:04 -0600

    v230826149

--- assets/navbar/js/navbar.js
+++ assets/navbar/js/navbar.js
@@ -43,13 +43,13 @@ const navTemplate = `
         </div>
         <ul class="menuN">
             <li class="linkN">
-                <a href="/pages/almirantes/index.html">Almirantes</a>
+                <a href="https://manuelheraz.github.io/MarinaGaming/pages/almirantes/index.html">Almirantes</a>
             </li>
             <li class="linkN">
-                <a href="/pages/moderadores/index.html">Moderadores</a>
+                <a href="https://manuelheraz.github.io/MarinaGaming/pages/moderadores/index.html">Moderadores</a>
             </li>
             <li class="linkN">
-                <a href="/pages/admins/index.html">Admins</a>
+                <a href="https://manuelheraz.github.io/MarinaGaming/pages/admins/index.html">Admins</a>
             </li>
         </ul>
     </li>
